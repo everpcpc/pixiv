4603b489118687f0fcbaa223648699026b3c69f1
Desk5103 <72625865+Desk5103@users.noreply.github.com>
2020-10-14 08:44:34 +0530

    New methods and specific file for exported json objects  (#4)
    * All exported json objects
    
    * Added new methods
    
    IllustComments
    IllustCommentAdd
    IllustRelated
    IllustRecommended
    IllustRanking
    TrendingTagsIllust
    SearchIllust
    IllustBookmarkDetail
    IllustBookmarkAdd
    IllustBookmarkDelete
    UserBookmarkTagsIllust
    UserFollowing
    UserFollower
    UserFollowAdd
    UserFollowDelete
    UserMyPixiv
    UserList
    UgoiraMetadata
    ShowcaseArticle

--- aapi.go
+++ aapi.go
@@ -21,76 +21,6 @@ func NewApp() *AppPixivAPI {
 	return &AppPixivAPI{sling: s}
 }
 
-type UserImages struct {
-	Medium string `json:"medium"`
-}
-type User struct {
-	ID         uint64 `json:"id"`
-	Name       string `json:"name"`
-	Account    string `json:"account"`
-	Comment    string `json:"comment"`
-	IsFollowed bool   `json:"is_followed"`
-
-	ProfileImages UserImages `json:"profile_image_urls"`
-}
-type UserDetail struct {
-	User *User `json:"user"`
-	// TODO:
-	// Profile
-	// ProfilePublicity
-	// Workspace
-}
-
-type Tag struct {
-	Name string `json:"name"`
-}
-type Images struct {
-	SquareMedium string `json:"square_medium"`
-	Medium       string `json:"medium"`
-	Large        string `json:"large"`
-	Original     string `json:"original"`
-}
-type MetaSinglePage struct {
-	OriginalImageURL string `json:"original_image_url"`
-}
-type MetaPage struct {
-	Images Images `json:"image_urls"`
-}
-type Illust struct {
-	ID          uint64   `json:"id"`
-	Title       string   `json:"title"`
-	Type        string   `json:"type"`
-	Images      Images   `json:"image_urls"`
-	Caption     string   `json:"caption"`
-	Restrict    int      `json:"restrict"`
-	User        User     `json:"user"`
-	Tags        []Tag    `json:"tags"`
-	Tools       []string `json:"tools"`
-	CreateData  string   `json:"create_data"`
-	PageCount   int      `json:"page_count"`
-	Width       int      `json:"width"`
-	Height      int      `json:"height"`
-	SanityLevel int      `json:"sanity_level"`
-	// TODO:
-	// Series `json:"series"`
-	MetaSinglePage MetaSinglePage `json:"meta_single_page"`
-	MetaPages      []MetaPage     `json:"meta_pages"`
-	TotalView      int            `json:"total_view"`
-	TotalBookmarks int            `json:"total_bookmarks"`
-	IsBookmarked   bool           `json:"is_bookmarked"`
-	Visible        bool           `json:"visible"`
-	IsMuted        bool           `json:"is_muted"`
-	TotalComments  int            `json:"total_comments"`
-}
-
-type illustsResponse struct {
-	Illusts []Illust `json:"illusts"`
-	NextURL string   `json:"next_url"`
-}
-type illustResponse struct {
-	Illust Illust `json:"illust"`
-}
-
 func (a *AppPixivAPI) request(path string, params, data interface{}, auth bool) (err error) {
 	if auth {
 		if _, err := refreshAuth(); err != nil {
@@ -99,6 +29,18 @@ func (a *AppPixivAPI) request(path string, params, data interface{}, auth bool) 
 		_, err = a.sling.New().Get(path).Set("Authorization", "Bearer "+_token).QueryStruct(params).ReceiveSuccess(data)
 	} else {
 		_, err = a.sling.New().Get(path).QueryStruct(params).ReceiveSuccess(data)
+	}
+	return err
+}
+
+func (a *AppPixivAPI) post(path string, params, data interface{}, auth bool) (err error) {
+	if auth {
+		if _, err := refreshAuth(); err != nil {
+			return fmt.Errorf("refresh token failed: %v", err)
+		}
+		_, err = a.sling.New().Post(path).Set("Authorization", "Bearer "+_token).BodyForm(params).ReceiveSuccess(data)
+	} else {
+		_, err = a.sling.New().Post(path).BodyForm(params).ReceiveSuccess(data)
 	}
 	return err
 }
@@ -139,7 +81,7 @@ func (a *AppPixivAPI) UserIllusts(uid uint64, _type string, offset int) ([]Illus
 		Type:   _type,
 		Offset: offset,
 	}
-	data := &illustsResponse{}
+	data := &IllustsResponse{}
 	if err := a.request(path, params, data, true); err != nil {
 		return nil, 0, err
 	}
@@ -165,7 +107,7 @@ func (a *AppPixivAPI) UserBookmarksIllust(uid uint64, restrict string, maxBookma
 		MaxBookmarkID: maxBookmarkID,
 		Tag:           tag,
 	}
-	data := &illustsResponse{}
+	data := &IllustsResponse{}
 	if err := a.request(path, params, data, true); err != nil {
 		return nil, 0, err
 	}
@@ -185,7 +127,7 @@ func (a *AppPixivAPI) IllustFollow(restrict string, offset int) ([]Illust, int, 
 		Restrict: restrict,
 		Offset:   offset,
 	}
-	data := &illustsResponse{}
+	data := &IllustsResponse{}
 	if err := a.request(path, params, data, true); err != nil {
 		return nil, 0, err
 	}
@@ -200,7 +142,7 @@ type illustDetailParams struct {
 // IllustDetail get a detailed illust with id
 func (a *AppPixivAPI) IllustDetail(id uint64) (*Illust, error) {
 	path := "v1/illust/detail"
-	data := &illustResponse{}
+	data := &IllustResponse{}
 	params := &illustDetailParams{
 		IllustID: id,
 	}
@@ -233,4 +175,399 @@ func (a *AppPixivAPI) Download(id uint64, path string) ([]int64, []error) {
 	}
 
 	return sizes, errs
+}
+
+type illustCommentsParams struct {
+	IllustID             uint64 `url:"illust_id,omitemtpy"`
+	Offset               int    `url:"offset,omitempty"`
+	IncludeTotalComments bool   `url:"include_total_comments,omitempty"`
+}
+
+// IllustComments Comments posted in a pixiv artwork
+func (a *AppPixivAPI) IllustComments(illustID uint64, offset int, includeTotalComments bool) (*IllustComments, error) {
+	path := "v1/illust/comments"
+	data := &IllustComments{}
+	params := &illustCommentsParams{
+		IllustID:             illustID,
+		IncludeTotalComments: includeTotalComments,
+		Offset:               offset,
+	}
+
+	if err := a.request(path, params, data, true); err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+type illustCommentAddParams struct {
+	IllustID        uint64 `url:"illust_id,omitempty"`
+	Comment         string `url:"comment,omitempty"`
+	ParentCommentID int    `url:"parent_comment_id,omitempty"`
+}
+
+// IllustCommentAdd adds a comment to given illustID
+func (a *AppPixivAPI) IllustCommentAdd(illustID uint64, comment string, parentCommentID int) (*IllustCommentAddResult, error) {
+	path := "v1/illust/comment/add"
+	data := &IllustCommentAddResult{}
+	params := &illustCommentAddParams{
+		IllustID:        illustID,
+		Comment:         comment,
+		ParentCommentID: parentCommentID,
+	}
+	if err := a.post(path, params, data, true); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+type illustRelatedParams struct {
+	IllustID      uint64   `url:"illust_id,omitempty"`
+	Filter        string   `url:"filter,omitempty"`
+	SeedIllustIDs []string `url:"seed_illust_ids[],omitempty,omitempty"`
+}
+
+// IllustRelated returns Related works
+func (a *AppPixivAPI) IllustRelated(illustID uint64, filter string, seedIllustIDs []string) (*IllustsResponse, error) {
+	path := "v2/illust/related"
+	data := &IllustsResponse{}
+	if filter == "" {
+		filter = "for_ios"
+	}
+	params := &illustRelatedParams{
+		IllustID: illustID,
+		Filter:   filter,
+	}
+	if seedIllustIDs != nil {
+		params.SeedIllustIDs = seedIllustIDs
+	}
+
+	if err := a.request(path, params, data, true); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+type illustRecommendedParams struct {
+	ContentType                  string   `url:"content_type,omitempty"`
+	IncludeRankingLabel          bool     `url:"include_ranking_label,omitempty"`
+	Filter                       string   `url:"filter,omitempty"`
+	MaxBookmarkIDForRecommended  string   `url:"max_bookmark_id_for_recommend,omitempty"`
+	MinBookmarkIDForRecentIllust string   `url:"min_bookmark_id_for_recent_illust,omitempty"`
+	Offset                       int      `url:"offset,omitempty"`
+	IncludeRankingIllusts        bool     `url:"include_ranking_illusts,omitempty"`
+	BookmarkIllustIDs            []string `url:"bookmark_illust_ids,omitempty"`
+	IncludePrivacyPolicy         string   `url:"include_privacy_policy,omitempty"`
+}
+
+// IllustRecommended Home Recommendation
+//
+//contentType: [illust, manga]
+func (a *AppPixivAPI) IllustRecommended(contentType string, includeRankingLabel bool, filter string, maxBookmarkIDForRecommended string, minBookmarkIDForRecentIllust string, offset int, includeRankingIllusts bool, bookmarkIllustIDs []string, includePrivacyPolicy string, requireAuth bool) (*IllustRecommended, error) {
+	path := "v1/illust/recommended-nologin"
+	if requireAuth == true {
+		path = "v1/illust/recommended"
+	}
+
+	data := &IllustRecommended{}
+
+	params := &illustRecommendedParams{
+		ContentType:                  contentType,
+		IncludeRankingLabel:          includeRankingLabel,
+		Filter:                       filter,
+		Offset:                       offset,
+		BookmarkIllustIDs:            bookmarkIllustIDs,
+		IncludePrivacyPolicy:         includePrivacyPolicy,
+		IncludeRankingIllusts:        includeRankingIllusts,
+		MaxBookmarkIDForRecommended:  maxBookmarkIDForRecommended,
+		MinBookmarkIDForRecentIllust: minBookmarkIDForRecentIllust,
+	}
+
+	if err := a.request(path, params, data, true); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+type illustRankingParams struct {
+	Mode   string `url:"mode,omitempty"`
+	Filter string `url:"filter,omitempty"`
+	Date   string `url:"date,omitempty"`
+	Offset string `url:"offset,omitempty"`
+}
+
+// IllustRanking Ranking of works
+//
+// mode: [day, week, month, day_male, day_female, week_original, week_rookie, day_manga]
+//
+// date: yyyy-mm-dd
+func (a *AppPixivAPI) IllustRanking(mode string, filter string, date string, offset string) (*IllustsResponse, error) {
+	path := "v1/illust/ranking"
+	data := &IllustsResponse{}
+	params := &illustRankingParams{
+		Mode:   mode,
+		Filter: filter,
+		Offset: offset,
+		Date:   date,
+	}
+	if err := a.request(path, params, data, true); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+type trendingTagsIllustParams struct {
+	Filter string `url:"filter,omitempty"`
+}
+
+// TrendingTagsIllust Trend label
+func (a *AppPixivAPI) TrendingTagsIllust(filter string) (*TrendingTagsIllust, error) {
+	path := "v1/trending-tags/illust"
+	data := &TrendingTagsIllust{}
+	params := &trendingTagsIllustParams{
+		Filter: filter,
+	}
+	if err := a.request(path, params, data, true); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+type searchIllustParams struct {
+	Word         string `url:"word,omitempty"`
+	SearchTarget string `url:"search_target,omitempty"`
+	Sort         string `url:"sort,omitempty"`
+	Filter       string `url:"filter,omitempty"`
+	Duration     string `url:"duration,omitempty"`
+	Offset       int    `url:"offset,omitempty"`
+}
+
+// SearchIllust search for
+//
+// searchTarget - Search type
+//	"partial_match_for_tags"  - The label part is consistent
+//	"exact_match_for_tags"    - The labels are exactly the same
+//	"title_and_caption"       - Title description
+//
+// sort: [date_desc, date_asc]
+//
+// duration: [within_last_day, within_last_week, within_last_month]
+func (a *AppPixivAPI) SearchIllust(word string, searchTarget string, sort string, duration string, filter string, offset int) (*SearchIllustResult, error) {
+	path := "v1/search/illust"
+	data := &SearchIllustResult{}
+	params := &searchIllustParams{
+		Word:         word,
+		SearchTarget: searchTarget,
+		Sort:         sort,
+		Filter:       filter,
+		Duration:     duration,
+		Offset:       offset,
+	}
+	if err := a.request(path, params, data, true); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+type illustBookmarkDetailParams struct {
+	IllustID uint64 `url:"illust_id,omitempty"`
+}
+
+// IllustBookmarkDetail Bookmark details
+func (a *AppPixivAPI) IllustBookmarkDetail(illustID uint64) (*IllustBookmarkDetail, error) {
+	path := "v2/illust/bookmark/detail"
+	data := &IllustBookmarkDetail{}
+	params := &illustBookmarkDetailParams{
+		IllustID: illustID,
+	}
+	if err := a.request(path, params, data, true); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+type illustBookmarkAddParams struct {
+	IllustID uint64   `url:"illust_id,omitempty"`
+	Restrict string   `url:"restrict,omitempty"`
+	Tags     []string `url:"tags,omitempty"`
+}
+
+// IllustBookmarkAdd Add bookmark
+func (a *AppPixivAPI) IllustBookmarkAdd(illustID uint64, restrict string, tags []string) error {
+	path := "v2/illust/bookmark/add"
+	params := illustBookmarkAddParams{
+		IllustID: illustID,
+		Restrict: restrict,
+	}
+	if tags != nil {
+		params.Tags = tags
+	}
+	return a.post(path, params, nil, true)
+}
+
+type illustBookmarkDeleteParams struct {
+	IllustID uint64 `url:"illust_id,omitempty"`
+}
+
+// IllustBookmarkDelete Remove bookmark
+func (a *AppPixivAPI) IllustBookmarkDelete(illustID uint64) error {
+	path := "v1/illust/bookmark/delete"
+	params := &illustBookmarkDeleteParams{
+		IllustID: illustID,
+	}
+	return a.post(path, params, nil, true)
+}
+
+type userBookmarkTagsIllustParams struct {
+	Restrict string
+	Offset   int
+}
+
+// UserBookmarkTagsIllust User favorite tag list
+func (a *AppPixivAPI) UserBookmarkTagsIllust(restrict string, offset int) (*UserBookmarkTags, error) {
+	path := "v1/user/bookmark-tags/illust"
+	data := &UserBookmarkTags{}
+	params := &userBookmarkTagsIllustParams{
+		Restrict: restrict,
+		Offset:   offset,
+	}
+	if err := a.request(path, params, data, true); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+type userFollowStatsParams struct {
+	UserID   uint64 `url:"user_id,omitempty"`
+	Restrict string `url:"restrict,omitempty"`
+	Offset   int    `url:"offset,omitempty"`
+}
+
+func userFollowStats(a *AppPixivAPI, urlEnd string, userID uint64, restrict string, offset int) (*UserFollowList, error) {
+	path := "v1/user/" + urlEnd
+	data := &UserFollowList{}
+	params := &userFollowStatsParams{
+		UserID:   userID,
+		Restrict: restrict,
+		Offset:   offset,
+	}
+	if err := a.request(path, params, data, true); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+// UserFollowing Following user list
+func (a *AppPixivAPI) UserFollowing(userID uint64, restrict string, offset int) (*UserFollowList, error) {
+	return userFollowStats(a, "following", userID, restrict, offset)
+}
+
+// UserFollower Follower user list
+func (a *AppPixivAPI) UserFollower(userID uint64, restrict string, offset int) (*UserFollowList, error) {
+	return userFollowStats(a, "follower", userID, restrict, offset)
+}
+
+type userFollowPostParams struct {
+	UserID   uint64 `url:"user_id,omitempty"`
+	Restrict string `url:"restrict,omitempty"`
+}
+
+func userFollowPost(a *AppPixivAPI, urlEnd string, userID uint64, restrict string) error {
+	path := "v1/user/follow/" + urlEnd
+	params := userFollowPostParams{
+		UserID:   userID,
+		Restrict: restrict,
+	}
+	return a.post(path, params, nil, true)
+}
+
+// UserFollowAdd Follow users
+func (a *AppPixivAPI) UserFollowAdd(userID uint64, restrict string) error {
+	return userFollowPost(a, "add", userID, restrict)
+}
+
+// UserFollowDelete Unfollow users
+func (a *AppPixivAPI) UserFollowDelete(userID uint64, restrict string) error {
+	return userFollowPost(a, "delete", userID, restrict)
+}
+
+type userMyPixivParams struct {
+	UserID uint64 `url:"user_id,omitempty"`
+	Offset int    `url:"offset,omitempty"`
+}
+
+// UserMyPixiv Users in MyPixiv
+func (a *AppPixivAPI) UserMyPixiv(userID uint64, offset int) (*UserFollowList, error) {
+	path := "/v1/user/mypixiv"
+	data := &UserFollowList{}
+	params := &userMyPixivParams{
+		UserID: userID,
+		Offset: offset,
+	}
+	if err := a.request(path, params, data, true); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+type userListParams struct {
+	UserID uint64 `url:"user_id,omitempty"`
+	Filter string `url:"filter,omitempty"`
+	Offset int    `url:"offset,omitempty"`
+}
+
+// UserList Blacklisted users
+func (a *AppPixivAPI) UserList(userID uint64, filter string, offset int) (*UserList, error) {
+	path := "v2/user/list"
+	data := &UserList{}
+	params := &userListParams{
+		UserID: userID,
+		Filter: filter,
+		Offset: offset,
+	}
+	if err := a.request(path, params, data, true); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+type ugoiraMetadataParams struct {
+	IllustID uint64 `url:"illust_id,omitempty"`
+}
+
+// UgoiraMetadata Ugoira Info
+func (a *AppPixivAPI) UgoiraMetadata(illustID uint64) (*UgoiraMetadata, error) {
+	path := "v1/ugoira/metadata"
+	data := &UgoiraMetadata{}
+	params := &ugoiraMetadataParams{
+		IllustID: illustID,
+	}
+	if err := a.request(path, params, data, true); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+type showcaseArticleParams struct {
+	ShowcaseID string `url:"article_id,omitempty"`
+}
+
+// ShowcaseArticle Special feature details (disguised as Chrome)
+func (a *AppPixivAPI) ShowcaseArticle(showcaseID string) (*ShowcaseArticle, error) {
+	base := "https://www.pixiv.net"
+	path := "ajax/showcase/article"
+
+	data := &ShowcaseArticle{}
+	params := &showcaseArticleParams{
+		ShowcaseID: showcaseID,
+	}
+
+	s := a.sling.New().Base(base + "/")
+	s.Set("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36")
+	s.Set("Referer", base)
+
+	if _, err := s.Get(path).QueryStruct(params).ReceiveSuccess(data); err != nil {
+		return nil, err
+	}
+	return data, nil
 }
