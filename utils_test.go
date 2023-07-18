cf0203897c91cfe1ae4557aad3ff51c62498c43c
everpcpc <git@everpcpc.com>
2023-07-18 10:09:27 +0800

    z

--- utils_test.go
+++ utils_test.go
@@ -6,9 +6,16 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestParseNextPageURL(t *testing.T) {
+func TestParseBookmarkNextPageURL(t *testing.T) {
 	r := require.New(t)
 	next, err := parseNextPageOffset("https://app-api.pixiv.net/v1/user/bookmarks/illust?filter=for_ios&restrict=private&user_id=60984430&max_bookmark_id=21354656694", OffsetFieldMaxBookmarkID)
 	r.Nil(err)
 	r.Equal(21354656694, next)
+}
+
+func TestParseOffsetNextPageURL(t *testing.T) {
+	r := require.New(t)
+	next, err := parseNextPageOffset("https://app-api.pixiv.net/v1/user/illusts?filter=for_ios&type=illust&user_id=490219&offset=30", OffsetFieldOffset)
+	r.Nil(err)
+	r.Equal(30, next)
 }
