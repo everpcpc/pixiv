049a79c19a46e4b6629ff7e22860b9a446d25b45
Miuzarte <982809597@qq.com>
2025-11-23 02:16:37 +0800

    feat: implmeent concurrent download support with context handling
    - Add context.Context to downloadImage()
    - Configurable thread count for concurrent download via SetThreads()
    - Add DownloadIllustIter() and DownloadIllustAsync() for streaming downloads
    - Extract getting urls from illust logic into illustGetUrls() helper func
    - Refactor downloadBytes() to downloadImage() returning structured Image type contains img data, page num and image format
    - Maintain backward compatibility with existing Download() and DownloadBytes() methods

--- utils_test.go
+++ utils_test.go
@@ -1,6 +1,7 @@
 package pixiv
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -33,3 +34,136 @@ func TestParseInvalidNextPageURL(t *testing.T) {
 	r.EqualError(err, "offset param omitted: invalid")
 	r.Equal(0, next)
 }
+
+func TestUrlToPageNum(t *testing.T) {
+	r := require.New(t)
+	urls := [...]string{
+		"https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/01/22/10/02/20/104714396_p0_square1200.jpg",
+		"https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/01/22/10/02/20/104714396_p1_master1200.jpg",
+		"https://i.pximg.net/img-original/img/2023/01/22/10/02/20/104714396_p2.jpg",
+	}
+	for i, url := range urls {
+		r.Equal(i, urlToPageNum(url))
+	}
+}
+
+func TestIllustGetUrlsMulti(t *testing.T) {
+	r := require.New(t)
+	illust := Illust{}
+	err := json.Unmarshal([]byte(TestIllustGetUrlsMultiJsonSample), &illust)
+	r.Nil(err)
+
+	expectedOriginal := []string{
+		"https://i.pximg.net/img-original/img/2023/01/22/10/02/20/104714396_p0.jpg",
+		"https://i.pximg.net/img-original/img/2023/01/22/10/02/20/104714396_p1.jpg",
+		"https://i.pximg.net/img-original/img/2023/01/22/10/02/20/104714396_p2.jpg",
+	}
+	urlsOriginal := illustGetUrls(&illust, SIZE_ORIGINAL)
+	r.Equal(expectedOriginal, urlsOriginal)
+
+	expectedLarge := []string{
+		"https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/01/22/10/02/20/104714396_p0_master1200.jpg",
+		"https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/01/22/10/02/20/104714396_p1_master1200.jpg",
+		"https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/01/22/10/02/20/104714396_p2_master1200.jpg",
+	}
+	urlsLarge := illustGetUrls(&illust, SIZE_LARGE)
+	r.Equal(expectedLarge, urlsLarge)
+
+	expectedMedium := []string{
+		"https://i.pximg.net/c/540x540_70/img-master/img/2023/01/22/10/02/20/104714396_p0_master1200.jpg",
+		"https://i.pximg.net/c/540x540_70/img-master/img/2023/01/22/10/02/20/104714396_p1_master1200.jpg",
+		"https://i.pximg.net/c/540x540_70/img-master/img/2023/01/22/10/02/20/104714396_p2_master1200.jpg",
+	}
+	urlsMedium := illustGetUrls(&illust, SIZE_MEDIUM)
+	r.Equal(expectedMedium, urlsMedium)
+
+	expectedSquareMedium := []string{
+		"https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/01/22/10/02/20/104714396_p0_square1200.jpg",
+		"https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/01/22/10/02/20/104714396_p1_square1200.jpg",
+		"https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/01/22/10/02/20/104714396_p2_square1200.jpg",
+	}
+	urlsSquareMedium := illustGetUrls(&illust, SIZE_SQUARE_MEDIUM)
+	r.Equal(expectedSquareMedium, urlsSquareMedium)
+}
+
+func TestIllustGetUrlsSingle(t *testing.T) {
+	r := require.New(t)
+	illust := Illust{}
+	err := json.Unmarshal([]byte(TestIllustGetUrlsSingleJsonSample), &illust)
+	r.Nil(err)
+
+	expectedOriginal := []string{
+		"https://i.pximg.net/img-original/img/2023/01/31/20/06/17/104968552_p0.jpg",
+	}
+	urlsOriginal := illustGetUrls(&illust, SIZE_ORIGINAL)
+	r.Equal(expectedOriginal, urlsOriginal)
+
+	expectedLarge := []string{
+		"https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/01/31/20/06/17/104968552_p0_master1200.jpg",
+	}
+	urlsLarge := illustGetUrls(&illust, SIZE_LARGE)
+	r.Equal(expectedLarge, urlsLarge)
+
+	expectedMedium := []string{
+		"https://i.pximg.net/c/540x540_70/img-master/img/2023/01/31/20/06/17/104968552_p0_master1200.jpg",
+	}
+	urlsMedium := illustGetUrls(&illust, SIZE_MEDIUM)
+	r.Equal(expectedMedium, urlsMedium)
+
+	expectedSquareMedium := []string{
+		"https://i.pximg.net/c/540x540_10_webp/img-master/img/2023/01/31/20/06/17/104968552_p0_square1200.jpg",
+	}
+	urlsSquareMedium := illustGetUrls(&illust, SIZE_SQUARE_MEDIUM)
+	r.Equal(expectedSquareMedium, urlsSquareMedium)
+}
+
+const TestIllustGetUrlsMultiJsonSample = `{
+    "image_urls": {
+        "square_medium": "https://i.pximg.net/c/540x540_10_webp/img-master/img/2023/01/22/10/02/20/104714396_p0_square1200.jpg",
+        "medium": "https://i.pximg.net/c/540x540_70/img-master/img/2023/01/22/10/02/20/104714396_p0_master1200.jpg",
+        "large": "https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/01/22/10/02/20/104714396_p0_master1200.jpg",
+        "original": ""
+    },
+    "meta_single_page": {
+        "original_image_url": ""
+    },
+    "meta_pages": [
+        {
+            "image_urls": {
+                "square_medium": "https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/01/22/10/02/20/104714396_p0_square1200.jpg",
+                "medium": "https://i.pximg.net/c/540x540_70/img-master/img/2023/01/22/10/02/20/104714396_p0_master1200.jpg",
+                "large": "https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/01/22/10/02/20/104714396_p0_master1200.jpg",
+                "original": "https://i.pximg.net/img-original/img/2023/01/22/10/02/20/104714396_p0.jpg"
+            }
+        },
+        {
+            "image_urls": {
+                "square_medium": "https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/01/22/10/02/20/104714396_p1_square1200.jpg",
+                "medium": "https://i.pximg.net/c/540x540_70/img-master/img/2023/01/22/10/02/20/104714396_p1_master1200.jpg",
+                "large": "https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/01/22/10/02/20/104714396_p1_master1200.jpg",
+                "original": "https://i.pximg.net/img-original/img/2023/01/22/10/02/20/104714396_p1.jpg"
+            }
+        },
+        {
+            "image_urls": {
+                "square_medium": "https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/01/22/10/02/20/104714396_p2_square1200.jpg",
+                "medium": "https://i.pximg.net/c/540x540_70/img-master/img/2023/01/22/10/02/20/104714396_p2_master1200.jpg",
+                "large": "https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/01/22/10/02/20/104714396_p2_master1200.jpg",
+                "original": "https://i.pximg.net/img-original/img/2023/01/22/10/02/20/104714396_p2.jpg"
+            }
+        }
+    ]
+}`
+
+const TestIllustGetUrlsSingleJsonSample = `{
+    "image_urls": {
+        "square_medium": "https://i.pximg.net/c/540x540_10_webp/img-master/img/2023/01/31/20/06/17/104968552_p0_square1200.jpg",
+        "medium": "https://i.pximg.net/c/540x540_70/img-master/img/2023/01/31/20/06/17/104968552_p0_master1200.jpg",
+        "large": "https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/01/31/20/06/17/104968552_p0_master1200.jpg",
+        "original": ""
+    },
+    "meta_single_page": {
+        "original_image_url": "https://i.pximg.net/img-original/img/2023/01/31/20/06/17/104968552_p0.jpg"
+    },
+    "meta_pages": []
+}`
